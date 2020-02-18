28b21e197fb6b527c61b6802ebddbeb16061e6c6
Artem Frolov <frolovart161@gmail.com>
2020-02-18 16:34:33 +0700

    All functionality in sigle thread complete

--- SharpPasswordManager/Views/DataView.xaml.cs
+++ SharpPasswordManager/Views/DataView.xaml.cs
@@ -24,5 +24,27 @@ namespace SharpPasswordManager.Views
         {
             InitializeComponent();
         }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            foreach (var item in ((sender as Button).Parent as Grid).Children)
+            {
+                if ((item as UIElement).Uid == "value")
+                    (item as TextBlock).Visibility = Visibility.Visible;
+                if ((item as UIElement).Uid == "mask")
+                    (item as TextBlock).Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            foreach (var item in ((sender as Button).Parent as Grid).Children)
+            {
+                if ((item as UIElement).Uid == "value")
+                    (item as TextBlock).Visibility = Visibility.Hidden;
+                if ((item as UIElement).Uid == "mask")
+                    (item as TextBlock).Visibility = Visibility.Visible;
+            }
+        }
     }
 }
