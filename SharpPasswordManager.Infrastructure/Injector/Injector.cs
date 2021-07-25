8c52b31f1116b871bcf3e3c14f6ad7a0d358f5bf
Artem Frolov <frolovart161@gmail.com>
2021-07-25 14:42:16 +0700

    Injector (DI mechanism) added

---
+++ SharpPasswordManager.Infrastructure/Injector/Injector.cs
@@ -0,0 +1,25 @@
+﻿namespace SharpPasswordManager.Infrastructure.Injector
+{
+    /// <summary>
+    /// Main DI app mechanism. Just simple singleton.
+    /// </summary>
+    public class Injector
+    {
+        public static Injector Instance { get; private set; }
+
+        static Injector()
+        {
+            Instance = new Injector();
+        }
+
+        private Injector()
+        {
+            InitDependencies();
+        }
+
+        private void InitDependencies()
+        {
+
+        }
+    }
+}
